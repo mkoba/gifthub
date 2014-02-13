237b5da73d45a41086e387cfbe0d0fb5451329d6
vagrant <vagrant@precise32.(none)>
2014-02-13 11:14:18 +0000

    yay databases

--- routes/eventpage.js
+++ routes/eventpage.js
@@ -1,5 +1,8 @@
 var data = require('../data.json');
 exports.view = function(req, res){
 	console.log(data);
-	res.render('eventpage', data);
+	var code = req.query.eventcode;
+	console.log(code);
+	console.log(data[code]);
+	res.render('eventpage', data[code]);
 }
